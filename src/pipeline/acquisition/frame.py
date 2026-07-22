ba982fee2a4f92fe25c15efd6ea4f8e40c863680
OPAL-DWB <lpag.oxford@gmail.com>
2026-07-22 17:02:58 +0100

    WIP: write frame.py

--- src/pipeline/acquisition/frame.py
+++ src/pipeline/acquisition/frame.py
@@ -5,12 +5,20 @@ This file encapsulates the FrameData class, containing raw frame data + metadata
 # Imports
 from dataclasses import dataclass
 import numpy as np
+import time
+
+from pipeline.utils.helpers import load_config
 
 # Constants
+config = load_config("configs/default.yaml")
+dtype_config = config["canonical_dtype"]["serial_number"]
+CANONICAL_DTYPE = np.dtype(dtype_config).type
+CANONICAL_NDIM = 2
+
 
 # Classes
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, slots=True, eq=False)
 class FrameData:
 
     '''
